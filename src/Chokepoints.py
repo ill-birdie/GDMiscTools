594cef2dd0391b9d216e4023b2a6d82aa4b8a305
Ava <avahom16@gmail.com>
2026-04-15 20:55:46 -0400

    Chokepoints.py inserting and graphing data

--- src/Chokepoints.py
+++ src/Chokepoints.py
@@ -1,25 +1,51 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 from Attempts import Attempts
 
 
 class Chokepoints:
     def __init__(self, attempts: dict[int, int]) -> None:
-        # Overwrites TypeError: expected non-empty vector for x
-        assert attempts, "No attempts given"
-
         # Avoids division by zero when calculating the log regression
-        attempts.pop(0)
+        if 0 in attempts:
+            attempts.pop(0)
 
         # Converts the attempts into numpy array local variables x and y
-        x, y = np.array(list(attempts.keys())), np.array(list(attempts.values()))
-        fit = np.polyfit(np.log(x), y, 1)
+        # TODO: all x values from 1 to 100 should be included, not just the one where deaths took place
+        try:
+            self._x, self._y = np.array(list(attempts.keys())), np.array(list(attempts.values()))
+            total_deaths = sum(self._y)
+            self._y = [n / total_deaths for n in self._y]
+            fit = np.polyfit(np.log(self._x), self._y, 1)
+        except ValueError:
+            raise TypeError("Attempt data must be purely runs from zero. Maybe the wrong data was inserted?")
+        except TypeError:
+            raise TypeError("Attempt data must contain at least one death entry.")
         self._a_fit, self._b_fit = fit[1], fit[0]
 
     @property
     def fit(self) -> str:
         return f"{round(self._a_fit, 3)} + {round(self._b_fit, 3)}ln(x)"
 
+    @property
+    def x(self):
+        return self._x
+
+    @property
+    def y(self):
+        return self._y
+
     def expected(self, percent) -> float:
         return self._a_fit + self._b_fit*np.log(percent)
+
+    def graph(self) -> None:
+        plt.plot(self._x, self._y)
+        expected_x = np.arange(1, 101, 1)
+        expected_y = [max(0.0, self.expected(n)) for n in np.arange(1, 101, 1)]
+        plt.plot(expected_x, expected_y)
+        plt.show()
+
+att = Attempts()
+cp = Chokepoints(att.from_zero)
+
+cp.graph()
