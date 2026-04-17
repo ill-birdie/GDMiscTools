0499f5e92d16829f7fb3268aacea17522d9cc1b0
Ava <avahom16@gmail.com>
2026-04-17 18:58:24 -0400

    Chokepoints.py logic completely reworked. Weighted average pass rate calculated

--- src/Chokepoints.py
+++ src/Chokepoints.py
@@ -1,51 +1,29 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-from Attempts import Attempts
-
 
 class Chokepoints:
     def __init__(self, attempts: dict[int, int]) -> None:
-        # Avoids division by zero when calculating the log regression
-        if 0 in attempts:
-            attempts.pop(0)
+        # Avoids division by zero when calculating for the weight of each percent
+        if not attempts:
+            raise TypeError("No attempts listed")
 
-        # Converts the attempts into numpy array local variables x and y
-        # TODO: all x values from 1 to 100 should be included, not just the one where deaths took place
-        try:
-            self._x, self._y = np.array(list(attempts.keys())), np.array(list(attempts.values()))
-            total_deaths = sum(self._y)
-            self._y = [n / total_deaths for n in self._y]
-            fit = np.polyfit(np.log(self._x), self._y, 1)
-        except ValueError:
-            raise TypeError("Attempt data must be purely runs from zero. Maybe the wrong data was inserted?")
-        except TypeError:
-            raise TypeError("Attempt data must contain at least one death entry.")
-        self._a_fit, self._b_fit = fit[1], fit[0]
-
-    @property
-    def fit(self) -> str:
-        return f"{round(self._a_fit, 3)} + {round(self._b_fit, 3)}ln(x)"
+        self._pass_rates = {}
+        self._avg_pass_rate = 0.0
+        death_total = sum(attempts.values())
+        reached = death_total
+        for percent, deaths in attempts.items():
+            passed = reached - deaths
+            pass_rate = (passed / reached) * 100
+            weight = deaths / death_total
+            self._avg_pass_rate += pass_rate*weight
+            self._pass_rates[percent] = pass_rate
+            reached = passed
 
     @property
-    def x(self):
-        return self._x
+    def pass_rates(self) -> dict[int, tuple]:
+        return self._pass_rates
 
     @property
-    def y(self):
-        return self._y
-
-    def expected(self, percent) -> float:
-        return self._a_fit + self._b_fit*np.log(percent)
-
-    def graph(self) -> None:
-        plt.plot(self._x, self._y)
-        expected_x = np.arange(1, 101, 1)
-        expected_y = [max(0.0, self.expected(n)) for n in np.arange(1, 101, 1)]
-        plt.plot(expected_x, expected_y)
-        plt.show()
-
-att = Attempts()
-cp = Chokepoints(att.from_zero)
-
-cp.graph()
+    def avg_pass_rate(self) -> float:
+        return self._avg_pass_rate
