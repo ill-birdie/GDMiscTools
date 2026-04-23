30d24448afd6e42f974a43b166af25d0ce8dedc0
Ava <avahom16@gmail.com>
2026-04-23 19:56:25 -0400

    Merged the initialization of self._from_zero and self._runs into one method

--- src/Attempts.py
+++ src/Attempts.py
@@ -7,11 +7,12 @@ class Attempts:
         self._curr_dir = Path.home() / "IdeaProjects" / "GeometryDash" / "src"
         self.name = level_name
 
-        # Helper functions insert data into from_zero and runs
         self._from_zero = {}
-        self.parse_from_zero()
         self._runs = {}
-        self.parse_runs()
+        from_zero_path = self._curr_dir / "FromZero.txt"
+        runs_path = self._curr_dir / "Runs.txt"
+        self.iter_file(from_zero_path, is_run=False)
+        self.iter_file(runs_path, is_run=True)
 
     def __str__(self) -> str:
         return f"AttemptHelper object {f"for level {self.name}" if self.name is not None else f"with {self.total_count} attempts"}"
@@ -34,7 +35,7 @@ class Attempts:
 
     @cached_property
     def total(self) -> dict[int | tuple, int]:
-        return self._from_zero | self.runs
+        return self._from_zero | self._runs
 
     @cached_property
     def from_zero_count(self) -> int:
@@ -48,54 +49,35 @@ class Attempts:
     def total_count(self) -> int:
         return self.from_zero_count + self.runs_count
 
-
-    def parse_from_zero(self) -> None:
-        """
-        Function goes line-by-line through "FromZero.txt" to add entries into self._from_zero
-        :return: None
-        """
-        path = self._curr_dir / "FromZero.txt"
+    def iter_file(self, path: Path, is_run: bool) -> None:
         if not path.exists():
-            print(f'File not found: "{path.name}"')
-            return
+            raise FileNotFoundError(f"File not found at {path}")
 
-        with open(path, mode="r") as file:
+        with open(path, "r") as file:
             for line in file:
-                if "%" in line:
-                    # The format of each line is "{percent}% x{num_deaths}" (ex. 5% x10)
-                    # Removes the space and the "%" in line
-                    line = line.strip().replace("%", " ").replace(" ", "")
+                line = line.strip()
+                if not line:
+                    continue
 
-                    # String now in the format "{percent}x{num_deaths}" (ex. 5x10)
-                    # Splits line across "x" into a list of integers
-                    line = [int(n) for n in line.split("x")]
-                    percent, num_deaths = line[0], line[1]
+                if is_run:
+                    if "-" in line:
+                        # The format of each line is "{run_start}% - {run_end}% x{num_deaths}" (ex. 5% - 25% x10)
+                        line = line.replace("x", " ").replace("-", " ").replace(" ", "")
 
-                    # Inserts data in the format "{percent}: {num_deaths}" (ex. {5: 10})
-                    self._from_zero[percent] = num_deaths
+                        # String now in the format "{run_start}%{run_end}%{num_deaths}" (ex. 5%25%10)
+                        line = [int(n) for n in line.split("%")]
+                        run_start, run_end, num_deaths = line[0], line[1], line[2]
 
+                        # Inserts data in the format "{({run_start}, {run_end}): {num_deaths})}" (ex. {(5, 25): 10})
+                        self._runs[(run_start, run_end)] = num_deaths
+                else:
+                    if "%" in line:
+                        # The format of each line is "{percent}% x{num_deaths}" (ex. 5% x10)
+                        line = line.replace("%", " ").replace(" ", "")
 
-    def parse_runs(self) -> None:
-        """
-        Function goes line-by-line through "Runs.txt" to add entries into self._runs
-        :return: None
-        """
-        path = self._curr_dir / "Runs.txt"
-        if not path.exists():
-            print(f'File not found: "{path.name}"')
-            return
+                        # String now in the format "{percent}x{num_deaths}" (ex. 5x10)
+                        line = [int(n) for n in line.split("x")]
+                        percent, num_deaths = line[0], line[1]
 
-        with open(path, mode="r") as file:
-            for line in file:
-                if "-" in line:
-                    # The format of each line is "{run_start}% - {run_end}% x{num_deaths}" (ex. 5% - 25% x10)
-                    # Removes the "-", the "x", and all instances of spaces, in line
-                    line = line.strip().replace("x", " ").replace("-", " ").replace(" ", "")
-
-                    # String now in the format "{run_start}%{run_end}%{num_deaths}" (ex. 5%25%10)
-                    # Splits line across instances of "%" into a list of integers
-                    line = [int(n) for n in line.split("%")]
-                    run_start, run_end, num_deaths = line[0], line[1], line[2]
-
-                    # Inserts data in the format "{({run_start}, {run_end}): {num_deaths})}" (ex. {(5, 25): 10})
-                    self._runs[(run_start, run_end)] = num_deaths
+                        # Inserts data in the format "{percent}: {num_deaths}" (ex. {5: 10})
+                        self._from_zero[percent] = num_deaths
