677dca2fbd3f7fd0be280381a128b87a58e6f435
Ava <avahom16@gmail.com>
2026-04-24 18:06:47 -0400

    Chokepoints.py code reformatting

--- src/Chokepoints.py
+++ src/Chokepoints.py
@@ -1,5 +1,8 @@
+from functools import cached_property
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.lines import lineStyles
+
 
 class Chokepoints:
     def __init__(self, attempts: dict[int, int]) -> None:
@@ -10,30 +13,25 @@ class Chokepoints:
         if 0 in death_percents:
             death_percents.pop(0)
 
-        self._x, self._y = np.arange(1, 101, 1), np.array(list(death_percents.values()))
+        self._x = np.arange(1, 101, 1)
+        self._y = np.array(list(death_percents.values()))
         fit = np.polyfit(np.log(self._x), self._y, deg=1)
 
         # Regression equation is a*ln(x) + b
         self._a_fit, self._b_fit = fit[1], fit[0]
 
-    @property
+    @cached_property
     def fit(self) -> str:
         return f"{round(self._a_fit, 3)} + {round(self._b_fit, 3)}ln(x)"
-
-    @property
-    def x(self):
-        return self._x
-
-    @property
-    def y(self):
-        return self._y
 
     def expected(self, percent) -> float:
         return self._a_fit + self._b_fit*np.log(percent)
 
+    @cached_property
+    def expected_y(self):
+        return [max(0.0, self.expected(percent)) for percent in range(1, 101)]
+
     def graph(self) -> None:
-        plt.plot(self._x, self._y)
-        expected_x = np.arange(1, 101, 1)
-        expected_y = [max(0.0, self.expected(n)) for n in np.arange(1, 101, 1)]
-        plt.plot(expected_x, expected_y)
+        plt.plot(self._x, self._y, color="orange")
+        plt.plot(self._x, self.expected_y, color="blue", ls="--", alpha=0.7)
         plt.show()
